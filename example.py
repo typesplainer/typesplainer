cff1d2adaadd264174211c089d1670c2f9f70e5e
Wasi Master <arianmollik323@gmail.com>
2022-03-14 11:21:12 +0600

    Diversify example callable

--- example.py
+++ example.py
@@ -1,12 +1,12 @@
 from typing import *
 
 class Text:
-    pass
+  pass
 
 def life(
-    f: Callable[[str], Generator[int, str, bool]],
-    g: Awaitable[Sequence[Text]]
+  f: Callable[[str, int], Generator[int, str, bool]],
+  g: Awaitable[Sequence[Text]]
 ) -> Dict[List[Set[FrozenSet[int]]], str]:
-    return {[{frozenset(42)}]: "Hello World!"}
+  return {[{frozenset(42)}]: "Hello World!"}
 
 fromkeys: Callable[[List[int]], Dict[int, List[Union[Segment, None]]]]
